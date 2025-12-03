a0074c39afee2c4295167637abd1fe915a40b8a2
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-12-03 15:53:57 +0100

    Script is executable

--- docs/lesson_plans/20251204/script_1.sh
+++ docs/lesson_plans/20251204/script_1.sh
@@ -4,3 +4,4 @@ wget https://raw.githubusercontent.com/UPPMAX/linux-command-line-201/refs/heads/
 sed --in-place 's/Weird Sisters/witches/g' macbeth.txt
 sed --in-place 's/[A-Z][a-z][a-z]*land/Sweden/' macbeth.txt
 sed --in-place '1,26d;4173,4495d' macbeth.txt
+
